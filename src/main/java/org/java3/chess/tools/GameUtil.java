26a70f0184dd9942c1e5b7357a9f8140d5eca79a
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-21 00:04:02 +0300

    added stockfish client and queries to the game util

--- src/main/java/org/java3/chess/tools/GameUtil.java
+++ src/main/java/org/java3/chess/tools/GameUtil.java
@@ -1,9 +1,14 @@
 package org.java3.chess.tools;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import xyz.niflheim.stockfish.StockfishClient;
+import xyz.niflheim.stockfish.engine.enums.Option;
+import xyz.niflheim.stockfish.engine.enums.Query;
+import xyz.niflheim.stockfish.engine.enums.QueryType;
+import xyz.niflheim.stockfish.engine.enums.Variant;
+import xyz.niflheim.stockfish.exceptions.StockfishInitException;
+
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -29,6 +34,20 @@ public final class GameUtil {
             Map.entry("p", "♟")
     );
 
+    private static final StockfishClient client;
+
+    static {
+        try {
+            client = new StockfishClient.Builder()
+                    .setInstances(4)
+                    .setOption(Option.Threads, 4)
+                    .setVariant(Variant.BMI2)
+                    .build();
+        } catch (StockfishInitException e) {
+            throw new RuntimeException("StockfishInitException: " + e.getMessage());
+        }
+    }
+
     public static List<List<String>> getBoard(String fen, Color color) {
         List<List<String>> whiteBoard = Arrays.stream(
                 Pattern.compile("[1-8]")
@@ -53,4 +72,33 @@ public final class GameUtil {
         return Integer.parseInt(fen.split("\\s")[5]);
     }
 
+    @SuppressWarnings("unchecked")
+    public static List<String> getLegalMoves(String fen) throws ExecutionException, InterruptedException {
+        return (List<String>) client.submit(
+                new Query.Builder(QueryType.Legal_Moves)
+                        .setFen(fen)
+                        .build(),
+                result -> Arrays.asList(result.split("\\s"))
+        ).get();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<String> getCheckers(String fen) throws ExecutionException, InterruptedException {
+        return (List<String>) client.submit(
+                new Query.Builder(QueryType.Checkers)
+                        .setFen(fen)
+                        .build(),
+                result -> Arrays.asList(result.split("\\s"))
+        ).get();
+    }
+
+    public static String makeMove(String fen, String move) throws ExecutionException, InterruptedException {
+        return (String) client.submit(
+                new Query.Builder(QueryType.Make_Move)
+                        .setFen(fen)
+                        .setMove(move)
+                        .build(),
+                result -> result
+        ).get();
+    }
 }

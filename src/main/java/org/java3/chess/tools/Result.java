afd488f35f3952c623cd793ea298590c33863305
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-21 00:02:49 +0300

    added game results

---
+++ src/main/java/org/java3/chess/tools/Result.java
@@ -0,0 +1,22 @@
+package org.java3.chess.tools;
+
+public enum Result {
+
+    WHITE_WON_BY_CHECKMATE("White won by checkmate"),
+    WHITE_WON_BY_RESIGNATION("White won by resignation"),
+    BLACK_WON_BY_CHECKMATE("Black won by checkmate"),
+    BLACK_WON_BY_RESIGNATION("Black won by resignation"),
+    DRAW_BY_FIFTY_MOVE_RULE("Draw by fifty move rule"),
+    DRAW_BY_STALEMATE("Draw by stalemate"),
+    DRAW_BY_INSUFFICIENT_MATERIAL("Draw by insufficient material");
+
+    private final String description;
+
+    Result(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}

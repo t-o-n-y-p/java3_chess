57d26361d96f0e92cbc97d943c8dd120205e2c09
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-23 01:15:37 +0300

    moved stockfish calls to StockfishUtil

--- src/main/java/org/java3/chess/tools/GameUtil.java
+++ src/main/java/org/java3/chess/tools/GameUtil.java
@@ -2,12 +2,6 @@ package org.java3.chess.tools;
 
 import org.java3.chess.model.Color;
 import org.java3.chess.model.User;
-import xyz.niflheim.stockfish.StockfishClient;
-import xyz.niflheim.stockfish.engine.enums.Option;
-import xyz.niflheim.stockfish.engine.enums.Query;
-import xyz.niflheim.stockfish.engine.enums.QueryType;
-import xyz.niflheim.stockfish.engine.enums.Variant;
-import xyz.niflheim.stockfish.exceptions.StockfishInitException;
 
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -35,20 +29,6 @@ public final class GameUtil {
             Map.entry("n", "♞"),
             Map.entry("p", "♟")
     );
-
-    private static final StockfishClient client;
-
-    static {
-        try {
-            client = new StockfishClient.Builder()
-                    .setInstances(4)
-                    .setOption(Option.Threads, 4)
-                    .setVariant(Variant.BMI2)
-                    .build();
-        } catch (StockfishInitException e) {
-            throw new RuntimeException("StockfishInitException: " + e.getMessage());
-        }
-    }
 
     public static List<List<String>> getBoard(String fen, Color color) {
         List<List<String>> whiteBoard = Arrays.stream(
@@ -86,34 +66,6 @@ public final class GameUtil {
         return Integer.parseInt(fen.replaceAll(".*[\\s]", ""));
     }
 
-    public static String getLegalMoves(String fen) throws ExecutionException, InterruptedException {
-        return (String) client.submit(
-                new Query.Builder(QueryType.Legal_Moves)
-                        .setFen(fen)
-                        .build(),
-                result -> result
-        ).get();
-    }
-
-    public static String getCheckers(String fen) throws ExecutionException, InterruptedException {
-        return (String) client.submit(
-                new Query.Builder(QueryType.Checkers)
-                        .setFen(fen)
-                        .build(),
-                result -> result
-        ).get();
-    }
-
-    public static String makeMove(String fen, String move) throws ExecutionException, InterruptedException {
-        return (String) client.submit(
-                new Query.Builder(QueryType.Make_Move)
-                        .setFen(fen)
-                        .setMove(move)
-                        .build(),
-                result -> result
-        ).get();
-    }
-
     public static Result getResult(String fen, String legalMoves, List<String> positionHistory)
             throws ExecutionException, InterruptedException {
         if (isDrawByInsufficientMaterial(fen)) {
@@ -127,7 +79,7 @@ public final class GameUtil {
         }
         if (!legalMoves.isBlank()) {
             return Result.UNDEFINED;
-        } else if (getCheckers(fen).isBlank()) {
+        } else if (StockfishUtil.getCheckers(fen).isBlank()) {
             return Result.DRAW_BY_STALEMATE;
         } else if (whiteToMove(fen)) {
             return Result.BLACK_WON_BY_CHECKMATE;

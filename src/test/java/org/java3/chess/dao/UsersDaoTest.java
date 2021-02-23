17ffcd67a521fb2ed709f39575b91959ac52eec9
t-o-n-y-p <42129780+t-o-n-y-p@users.noreply.github.com>
2021-02-23 21:41:05 +0300

    added findByRatingAndLoginInput test

--- src/test/java/org/java3/chess/dao/UsersDaoTest.java
+++ src/test/java/org/java3/chess/dao/UsersDaoTest.java
@@ -9,7 +9,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +20,7 @@ public class UsersDaoTest {
     private EntityManagerFactory factory;
     private EntityManager manager;
     private UsersDao usersDao;
+    private final List<User> allCreatedUsers = new ArrayList<>();
 
     @Before
     public void setUp() {
@@ -27,9 +30,12 @@ public class UsersDaoTest {
 
         manager.getTransaction().begin();
         for (int i = 0; i < 120; i++) {
-            User user = new User("login" + i, "password" + i);
-            user.setRating(2800 - i * 15);
-            manager.persist(user);
+            for (String prefix : List.of("login", "test")) {
+                User user = new User(prefix + i, "password" + i);
+                user.setRating(2800 - i * 15);
+                allCreatedUsers.add(user);
+                manager.persist(user);
+            }
         }
         manager.getTransaction().commit();
     }
@@ -66,21 +72,37 @@ public class UsersDaoTest {
 
     @Test
     public void findByRating() {
-        assertTrue(usersDao.findByRating(3000, 50, 0, 100).isEmpty());
-        assertTrue(usersDao.findByRating(800, 50, 0, 100).isEmpty());
+        assertTrue(usersDao.findByRating(3000, 50, 0, 1000).isEmpty());
+        assertTrue(usersDao.findByRating(800, 50, 0, 1000).isEmpty());
 
-        assertFalse(usersDao.findByRating(3000, 400, 0, 100).isEmpty());
-        for (User c : usersDao.findByRating(3000, 400, 0, 100)) {
-            assertTrue(c.getRating() >= 2600 && c.getRating() <= 3400);
-        }
-        assertFalse(usersDao.findByRating(800, 400, 0, 100).isEmpty());
-        for (User c : usersDao.findByRating(800, 400, 0, 100)) {
-            assertTrue(c.getRating() >= 400 && c.getRating() <= 1200);
-        }
-        assertFalse(usersDao.findByRating(1900, 50, 0, 100).isEmpty());
-        for (User c : usersDao.findByRating(1900, 50, 0, 100)) {
-            assertTrue(c.getRating() >= 1850 && c.getRating() <= 1950);
-        }
+        List<User> result = usersDao.findByRating(3000, 400, 0, 1000);
+        assertFalse(result.isEmpty());
+        List<User> expectedUsers = allCreatedUsers.stream()
+                .filter(u -> u.getRating() >= 2600 && u.getRating() <= 3400)
+                .collect(Collectors.toList());
+        assertEquals(expectedUsers.size(), result.size());
+        assertTrue(result.containsAll(expectedUsers));
+        result = usersDao.findByRating(800, 400, 0, 1000);
+        assertFalse(result.isEmpty());
+        expectedUsers = allCreatedUsers.stream()
+                .filter(u -> u.getRating() >= 400 && u.getRating() <= 1200)
+                .collect(Collectors.toList());
+        assertEquals(expectedUsers.size(), result.size());
+        assertTrue(result.containsAll(expectedUsers));
+        result = usersDao.findByRating(1900, 50, 0, 1000);
+        assertFalse(result.isEmpty());
+        expectedUsers = allCreatedUsers.stream()
+                .filter(u -> u.getRating() >= 1850 && u.getRating() <= 1950)
+                .collect(Collectors.toList());
+        assertEquals(expectedUsers.size(), result.size());
+        assertTrue(result.containsAll(expectedUsers));
+        result = usersDao.findByRating(1900, 400, 0, 1000);
+        assertFalse(result.isEmpty());
+        expectedUsers = allCreatedUsers.stream()
+                .filter(u -> u.getRating() >= 1500 && u.getRating() <= 2300)
+                .collect(Collectors.toList());
+        assertEquals(expectedUsers.size(), result.size());
+        assertTrue(result.containsAll(expectedUsers));
 
         List<User> result1 = usersDao.findByRating(1900, 400, 0, 10);
         assertEquals(10, result1.size());
@@ -93,13 +115,64 @@ public class UsersDaoTest {
 
         result1 = usersDao.findByRating(1900, 400, 0, 20);
         assertEquals(20, result1.size());
-        result2 = usersDao.findByRating(1900, 400, 40, 20);
-        assertEquals(13, result2.size());
+        result2 = usersDao.findByRating(1900, 400, 100, 20);
+        assertEquals(6, result2.size());
         result1.retainAll(result2);
         assertTrue(result1.isEmpty());
     }
 
     @Test
     public void findByRatingAndLoginInput() {
+        assertTrue(usersDao.findByRatingAndLoginInput("login", 3000, 50, 0, 100).isEmpty());
+        assertTrue(usersDao.findByRatingAndLoginInput("login", 800, 50, 0, 100).isEmpty());
+        assertTrue(usersDao.findByRatingAndLoginInput("login_test", 1900, 50, 0, 100).isEmpty());
+
+        for (String input : List.of("login", "test")) {
+            List<User> result = usersDao.findByRatingAndLoginInput(input, 3000, 400, 0, 100);
+            assertFalse(result.isEmpty());
+            List<User> expectedUsers = allCreatedUsers.stream()
+                    .filter(u -> u.getRating() >= 2600 && u.getRating() <= 3400 && u.getLogin().contains(input))
+                    .collect(Collectors.toList());
+            assertEquals(expectedUsers.size(), result.size());
+            assertTrue(result.containsAll(expectedUsers));
+            result = usersDao.findByRatingAndLoginInput(input, 800, 400, 0, 100);
+            assertFalse(result.isEmpty());
+            expectedUsers = allCreatedUsers.stream()
+                    .filter(u -> u.getRating() >= 400 && u.getRating() <= 1200 && u.getLogin().contains(input))
+                    .collect(Collectors.toList());
+            assertEquals(expectedUsers.size(), result.size());
+            assertTrue(result.containsAll(expectedUsers));
+            result = usersDao.findByRatingAndLoginInput(input, 1900, 50, 0, 100);
+            assertFalse(result.isEmpty());
+            expectedUsers = allCreatedUsers.stream()
+                    .filter(u -> u.getRating() >= 1850 && u.getRating() <= 1950 && u.getLogin().contains(input))
+                    .collect(Collectors.toList());
+            assertEquals(expectedUsers.size(), result.size());
+            assertTrue(result.containsAll(expectedUsers));
+            result = usersDao.findByRatingAndLoginInput(input, 1900, 400, 0, 100);
+            assertFalse(result.isEmpty());
+            expectedUsers = allCreatedUsers.stream()
+                    .filter(u -> u.getRating() >= 1500 && u.getRating() <= 2300 && u.getLogin().contains(input))
+                    .collect(Collectors.toList());
+            assertEquals(expectedUsers.size(), result.size());
+            assertTrue(result.containsAll(expectedUsers));
+
+            List<User> result1 = usersDao.findByRatingAndLoginInput(input, 1900, 400, 0, 10);
+            assertEquals(10, result1.size());
+            List<User> result2 = usersDao.findByRatingAndLoginInput(input, 1900, 400, 40, 10);
+            assertEquals(10, result2.size());
+            result1.retainAll(result2);
+            assertTrue(result1.isEmpty());
+
+            assertTrue(usersDao.findByRatingAndLoginInput(input, 1900, 400, 1000, 10).isEmpty());
+
+            result1 = usersDao.findByRatingAndLoginInput(input, 1900, 400, 0, 20);
+            assertEquals(20, result1.size());
+            result2 = usersDao.findByRatingAndLoginInput(input, 1900, 400, 40, 20);
+            assertEquals(13, result2.size());
+            result1.retainAll(result2);
+            assertTrue(result1.isEmpty());
+        }
+
     }
 }
